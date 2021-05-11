aa3e12e152ca6f5456ef51e7e654802cab6be772
willjsantos <wjs@discente.ifpe.edu.br>
2021-05-11 17:35:07 -0300

    Create Questao-013.c
    adiciona questao 13 - operadores

---
+++ Mini-avaliacoes/Questao-013.c
@@ -0,0 +1,75 @@
+/* Operadores Questão 13
+
+Autor: Willian J. dos Santos
+ano:   2021 
+
+13) Faça um programa em C que receba um inteiro de 1 a 12 e retorne qual o mes do ano.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int mes = 0;
+    printf("Entre com um numero de 1 a 12: ");
+    scanf("%d", &mes);
+    
+    switch (mes){
+        case 1:
+        printf("\nJaneiro");
+        break;
+    
+        case 2:
+        printf("\nFevereiro");
+        break;
+
+        case 3:
+        printf("\nMarco");
+        break;
+
+        case 4:
+        printf("\nAbril");
+        break;
+        
+        case 5:
+        printf("\nMaio");
+        break;
+        
+        case 6:
+        printf("\nJunho");
+        break;
+        
+        case 7:
+        printf("\nJulho");
+        break;
+        
+        case 8:
+        printf("\nAgosto");
+        break;
+        
+        case 9:
+        printf("\nSetembro");
+        break;
+        
+        case 10:
+        printf("\nOutubro");
+        break;
+        
+        case 11:
+        printf("\nNovembro");
+        break;
+        
+        case 12:
+        printf("\nDezembro");
+        break;
+        
+        default:
+        printf("\nNumero fora do intervalo");
+        break;
+        
+    }
+     
+    return 0;
+}
