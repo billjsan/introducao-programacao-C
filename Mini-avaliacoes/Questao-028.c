dec289cba590ab17d5dcce1071684aa38bf43327
willjsantos <wjs@discente.ifpe.edu.br>
2021-05-22 18:23:59 -0300

    correcao dos nomes dos arquivos 28 e 29 - permuta

--- Mini-avaliacoes/Questao-028.c
+++ Mini-avaliacoes/Questao-028.c
@@ -1,40 +1,89 @@
-/* Operadores Questão 29
+/* Operadores Questão 28
 
 Autor: Willian J. dos Santos
 ano:   2021 
 
-29)  Criar uma função que receba um vetor de caracteres
-(palavra, vetor de char) e retorne a quantidade de caracteres
+28)  Tendo como base na aplicação acima, crie um programa que calcule o valor 
+total da poupança que além do valor inicial e valor do juros, também receba 
+valores de aplicações mensais (ex.: R$50,00/mês) durante todo o período da 
+sua idade até os 65 anos.
 */
 
 #include <stdio.h>
 
-#define SIZE 40
-
-int countString(char[SIZE]);
-
+//prototipos das funcoes
+double Pot (float base, int expoente);
+float juros (float montante, float _taxa, int _tempo, float aplicacoes);
+//funcao principal 
 int main()
 {
-
-    char palavra[SIZE] = {0};
-
     
-    printf("\nDigite uma palavra: ");
-    scanf("%39s", palavra);
+float capital, Tx, aplicacaoM = 0;
+
+int idade, meses = 0;
+
+printf("\nEntre com o valor inicial da aplicação: ");
+scanf("%f",&capital);
+    while(capital< 0){
+    printf("\nO valor deve ser positivo!");
+    printf("\nEntre com o valor inicial da aplicação: ");
+    scanf("%f",&capital);
+    }
+printf("\nEntre com a taxa de juros ao mes: ");
+scanf("%f",&Tx);
+    while(Tx< 0){
+    printf("\nO valor deve ser positivo!");
+    printf("\nEntre com a taxa de juros ao mes: ");
+    scanf("%f",&Tx);
+    }
+printf("\nEntre com a idade atual: ");
+scanf("%d",&idade);
+    while(idade< 0){
+    printf("\nO valor deve ser positivo!");
+    printf("\nEntre com a quantidade de meses: ");
+    scanf("%d",&idade);
+    }
+    
+    printf("\nDeseja realizar alguma aplicacao mensal no periodo ate a aposentadoria? sim = 1 nao = 0 ");
+    int Q = 0;
+    scanf("%d",&Q);
+    
+        if(Q == 1){
+        printf("\nEntre com o valor da aplicacao:");
+        scanf("%f",&aplicacaoM);
         
-    printf("\nVoce digitou uma palavra com: %d letras ", countString(palavra));
+            while(aplicacaoM<0){
+                
+            printf("\nO valor deve ser positivo!");
+            printf("\nEntre com o valor da aplicacao:");
+            scanf("%f",&aplicacaoM);
+            }
+        }
+        
+meses = (65-idade)*12;   
+printf("\nO montante apos %d meses sera: R$ %.2f", meses, juros(capital,Tx,idade,aplicacaoM));
 
     return 0;
 }
 
-
-int countString(char *palavra){
-     
-    int cnt = 0;
+//função do calculo do juros
+float juros (float P, float taxa, int tempo, float depositos){
     
-    while(palavra[cnt] != '\0'){
-    cnt++;    
-    }
-    return cnt;
+float aux = (1 +(taxa/100));
+int aux2 = (65 - tempo) * 12 ;//variavel auxiliar para tratar os meses
+float sumAplica = depositos * aux2;
+
+double montante = Pot(aux,aux2) * (P + sumAplica);
+return montante;    
 }
 
+//função de potência
+double Pot (float _base, int _exp){
+    
+double aux2 = 1.0;
+
+    for(int i = 0; i<_exp; i++){
+        aux2 = aux2 * _base ;
+    }
+return aux2;
+}
